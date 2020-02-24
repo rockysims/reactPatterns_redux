06a5168c909066382d2d8fcf801151d9b3a57ee9
Rocky Sims <rocky.sims@gmail.com>
2020-02-24 02:04:51 -0800

    Further simplified my redux/index.js file (perhaps over simplified?).

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,56 +1,39 @@
 
-const actByType = {};
+const reduceByType = {};
 
 let nextId = 201;
-actByType['addTodo'] = {
-	buildPayload: title => ({
+reduceByType['addTodo'] = (state, payload) => {
+	const [ title ] = payload;
+	const newItem = {
+		userId: 1,
 		id: nextId++,
-		title
-	}),
-	reduce: (state, payload) => {
-		const { id, title } = payload;
-		const newItem = {
-			userId: 1,
-			id,
-			title,
-			completed: false
-		};
-		return {
-			...state,
-			items: [...state.items, newItem]
-		};
-	}
+		title,
+		completed: false
+	};
+	return {
+		...state,
+		items: [...state.items, newItem]
+	};
 };
 
-actByType['removeTodo'] = {
-	buildPayload: id => ({
-		id
-	}),
-	reduce: (state, payload) => {
-		const { id } = payload;
-		return {
-			...state,
-			items: state.items.filter(item => item.id !== id)
-		};
-	}
+reduceByType['removeTodo'] = (state, payload) => {
+	const [ id ] = payload;
+	return {
+		...state,
+		items: state.items.filter(item => item.id !== id)
+	};
 };
 
-actByType['setTodoCompleted'] = {
-	buildPayload: (id, completed) => ({
-		id,
-		completed
-	}),
-	reduce: (state, payload) => {
-		const { id, completed } = payload;
-		return {
-			...state,
-			items: state.items.map(item => 
-				(item.id === id)
-					? { ...item, completed }
-					: item
-			)
-		};
-	}
+reduceByType['setTodoCompleted'] = (state, payload) => {
+	const [ id, completed ] = payload;
+	return {
+		...state,
+		items: state.items.map(item => 
+			(item.id === id)
+				? { ...item, completed }
+				: item
+		)
+	};
 };
 
 const initialState = {
@@ -59,21 +42,21 @@ const initialState = {
 
 // --- //
 
-export const actions = Object.fromEntries(
-	Object.keys(actByType).map(type => [
-		type,
-		(...args) => {
-			return {
-				type,
-				payload: actByType[type].buildPayload(...args)
-			};
-		}
-	])
-);
-
 export const rootReducer = (state = initialState, { type, payload }) => {
-	const reduce = actByType[type]?.reduce;
+	const reduce = reduceByType[type];
 	return reduce
 		? reduce(state, payload)
 		: state;
 };
+
+export const actions = Object.fromEntries(
+	Object.keys(reduceByType).map(type => [
+		type,
+		(...payload) => {
+			return {
+				type,
+				payload
+			};
+		}
+	])
+);
