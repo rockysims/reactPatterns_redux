83db9584d10a570984d614f26bd6cd549d0bf015
Rocky Sims <rocky.sims@gmail.com>
2020-02-23 23:27:46 -0800

    Minor formatting tweak.

--- src/redux/reducers/root.js
+++ src/redux/reducers/root.js
@@ -7,9 +7,7 @@ const initialState = {
 export default function(state = initialState, action) {
 	switch (action.type) {
 		case ADD_TODO: {
-			const {
-				id, title
-			} = action.payload;
+			const { id, title } = action.payload;
 			const newItem = {
 				userId: 1,
 				id,
